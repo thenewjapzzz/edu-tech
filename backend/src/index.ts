809513bf253ff925468784569dce191e2bcf4fa8
thenewjapzzz <guilherme.yokota20@gmail.com>
2024-09-29 03:35:06 -0300

    autenticação com usuário 'teacher'

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express from 'express';
 import AppDataSource from './config/database';
+import { User } from './models/userModel';
 import router from './router/userRoutes';
 
 const app = express();
@@ -9,24 +10,14 @@ app.use(express.json());
 const PORT = 3000;
 
 AppDataSource.initialize()
-    .then(() => {
+    .then(async () => {
         console.log('Conexão com o banco de dados estabelecida com sucesso.');
 
-        // Defina suas rotas aqui
-        app.get('/', (req, res) => {
-            res.send('Servidor rodando!');
-        });
-
-        app.use('/auth', router);
+        app.use('/', router)
 
         // Iniciando o servidor
         app.listen(PORT, () => {
             console.log(`Servidor rodando na porta ${PORT}`);
-        });
-
-        // Chama a função para criar o usuário após a inicialização do banco de dados
-        import('./utils/createTeacherUser').then(module => {
-            module.createTeacherUser().catch(error => console.log('Erro ao criar usuário: ', error));
         });
     })
     .catch((error) => console.log('Erro ao conectar ao banco de dados: ', error));
--- backend/src/router/userRoutes.ts
+++ backend/src/router/userRoutes.ts
@@ -1,8 +1,14 @@
 import AuthController from "../controllers/AuthController";
 import router from "./defaultRoutes";
 
-router.post('/', async(req, res) => {
-    await AuthController.loginUser(req, res)
-})
+router.post('/', async (req, res) => { 
+    try {
+        await AuthController.loginUser(req, res);
+    } catch (error) {
+        console.error('Erro ao logar usuário:', error);
+        res.status(500).send('Erro ao logar usuário.');
+    }
+});
+
 
 export default router;
